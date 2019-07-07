24e67846c3227ae56ffe8e0a8cc9388f999b772d
Randy Oest <oest@amazingrando.com>
2019-07-07 16:49:06 -0400

    Layout

--- wordpress/wp-content/themes/offguardgames/functions.php
+++ wordpress/wp-content/themes/offguardgames/functions.php
@@ -63,7 +63,8 @@ class StarterSite extends Timber\Site {
 		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu'] = new Timber\Menu();
+		$context['primaryMenu'] = new Timber\Menu(2);
+		$context['secondaryMenu'] = new \Timber\Menu(3);
 		$context['site'] = $this;
 		return $context;
 	}
--- wordpress/wp-content/themes/offguardgames/stories/blocks/blocks.stories.js
+++ wordpress/wp-content/themes/offguardgames/stories/blocks/blocks.stories.js
@@ -5,3 +5,10 @@ storiesOf("Gutenberg Blocks", module).add(
   () =>
     `<figure class="wp-block-image"><img src="http://offguardwp.lndo.site/wp-content/uploads/2019/07/firefly_2002_1841_poster.png" alt="" class="wp-image-35" srcset="http://offguardwp.lndo.site/wp-content/uploads/2019/07/firefly_2002_1841_poster.png 917w, http://offguardwp.lndo.site/wp-content/uploads/2019/07/firefly_2002_1841_poster-300x131.png 300w, http://offguardwp.lndo.site/wp-content/uploads/2019/07/firefly_2002_1841_poster-768x335.png 768w" sizes="(max-width: 917px) 100vw, 917px"><figcaption>This is a caption.</figcaption></figure>`
 );
+storiesOf("Gutenberg Blocks", module).add(
+  "Cover Image",
+  () =>
+    `<div class="wp-block-cover has-background-dim" style="background-image:url(http://offguardwp.lndo.site/wp-content/uploads/2019/07/firefly_2002_1841_poster.png)"><div class="wp-block-cover__inner-container">
+    <p style="text-align:center" class="has-large-font-size">Cover Text Cover Text Cover Text Cover Text Cover Text Cover Text Cover Text Cover Text Cover Text </p>
+    </div></div>`
+);
